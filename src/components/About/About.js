47fdba3620b04003be2a7c8ead965ed49d719b1b
lionelkvn <lionel.natividad@gmail.com>
2023-08-30 14:32:39 +0800

    added an about and installed packages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import "./About.css"
+import React from 'react';
+import "./About.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+
 
 const About = () => {
   return (
-    <div className='about-container'>
-        <div className='about-text' >
-            <div className="about-me-header">
-                <h1>About <span>Me</span></h1>
-            </div>
-           <div className='about-me-description' >
-            <h3> Hi! I'm a web developer from Pampanga, Philippines with a passion for creating pleasing and useful web applications. 
-            </h3>
-            </div>
+    <div className='about-container' id="about">
+      <div className='about-text'>
+        <div className="about-me-header">
+          <h1>About <span>Me</span></h1>
         </div>
+        <div className='about-me-description'>
+          <h3> Hi! I'm a web developer from Pampanga, Philippines with a passion for creating pleasing and useful web applications. 
+          </h3>
+        </div>
+      </div>
+      
+      <div className='techstack-container'>
+        <div className="techstack-text">
+          <h1>Tech<span>Stack</span></h1>
+            <div className="techstack-icons">
+              <div className="techstack-icon">
+              <FontAwesomeIcon icon={faHtml5} style={{ color: "#6600ff" }} />
 
-     </div>
+              </div>
+          
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
-export default About
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         </div>
         <div className='home-text'>
             <h1>I'm Lionel, a <span>Web Developer</span></h1>
-            <h2>and i create things for the web.</h2>
+            <h2>and I create things for the web.</h2>
             <br/>
             <button><h3>Get in touch </h3></button>
         </div>
