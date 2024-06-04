9e390616c66ae438985bfee73f0fc1805ed76875
lionelkvn <lionel.natividad@gmail.com>
2024-06-04 23:43:39 +0800

    edited my about and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,12 @@ const Footer = () => {
   return (
     <div className='footer-wrapper'>
         <div className="footer-container">
-        <p>© Lionel Natividad 2023 </p>
+        <p>© Lionel Natividad 2024 </p>
             <div className="socials">
                 <a href="https://github.com/lionelkvn"><img className="socials-icon"src="./images/github.png" alt="github-logo" /></a>
                 <a href="https://www.messenger.com/t/100000136477401/"><img className='socials-icon' src="./images/messenger.png" alt="x-logo" /></a>
                 <a href="https://www.facebook.com/lionel.natividad/"><img src="./images/facebook.png" className='socials-icon' alt="" /></a>
+                <p>+63 906 351 9099</p>
             </div>
         </div>
         
